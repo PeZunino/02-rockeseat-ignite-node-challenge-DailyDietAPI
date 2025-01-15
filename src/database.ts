270b787a0661b71fdc6d631aca0c375f6c3da7cb
PeZunino <pedrozsantos@hotmail.com>
2025-01-15 12:31:11 -0300

    feat(config): configure Knex with SQLite3

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import knex, {Knex} from 'knex';
+
+export const config: Knex.Config = {
+	client: 'sqlite3',
+	connection: {filename: './db/app.db',},
+	useNullAsDefault: true,
+	migrations:{
+		extension: 'ts',
+		directory:'./db/migrations'
+	}
+};
+
+export default knex(config);
